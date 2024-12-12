bdfb53f367f06366e5852ef3f7dd176941d4e4bc
AxelSolis93 <asolisz@unsa.edu.pe>
2024-12-12 02:49:06 -0500

    Avance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,9 +46,12 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.tesseract4android)
 }
