25de87009f61da44c6ddaefa8363137e5cc69493
git_n3wbie <jhapazac@unsa.edu.pe>
2024-12-26 10:02:37 -0500

    Altoque causa

---
+++ app/src/main/java/com/example/alea/ui/home/HomeFragment.kt
@@ -0,0 +1,57 @@
+package com.example.alea.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.alea.databinding.FragmentHomeBinding
+import androidx.navigation.fragment.findNavController
+import com.example.alea.R
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        /*
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+         */
+
+        // Configuración del botón para navegar al VoiceFragment
+
+        binding.buttonToVoiceFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_home_to_voiceFragment)
+        }
+
+        binding.buttonToCaloriesFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_home_to_caloriasFragment)
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
